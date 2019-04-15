29f8b21b5f0bd0c67b1beb43d5da87733a391613
Kognise <felix.mattick@gmail.com>
2019-04-15 00:22:18 -0400

    oooof

--- heart/index.js
+++ heart/index.js
@@ -1,4 +1,4 @@
-const uuid = require('uuid/v4')
+const uuid = (a) => a ? (a ^ Math.random() * 16 >> a / 4).toString(16):([ 1e7 ] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b)
 const express = require('express')
 const app = express()
 
