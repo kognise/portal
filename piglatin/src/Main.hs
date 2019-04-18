c0a33cdde13d3a80ddead6f4a0d08b566d11a4d0
JackFly26 <jackfleishman@outlook.com>
2019-04-18 18:26:56 -0400

    add GET support

--- piglatin/src/Main.hs
+++ piglatin/src/Main.hs
@@ -9,16 +9,19 @@ import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
 
+replace :: Eq a => a -> a -> [a] -> [a]
+replace a b = map $ \c -> if c == a then b else c
+
 main :: IO ()
-main = httpServe (setPort 80 mempty) site
+main = httpServe (setPort 80 mempty) site <|> quickHttpServe site
 
 site :: Snap ()
 site = route [
-        ("piglatin", method POST piglatin)
+        ("piglatin", piglatin)
     ] <|>
     ifTop (serveDirectory "./static")
 
 piglatin :: Snap ()
 piglatin = do
     param <- getParam "str"
-    writeText $ T.pack . unwords . fmap wordToPl . words . T.unpack . decodeUtf8 . fromJust $  param where wordToPl = (\(x, y) -> y ++ x ++ "ay") . break (`elem` ("aeiouAEIOU" :: String))
+    writeText $ T.pack . unwords . fmap wordToPl . words . replace '+' ' ' . T.unpack . decodeUtf8 . fromJust $  param where wordToPl = (\(x, y) -> y ++ x ++ "ay") . break (`elem` ("aeiouAEIOU" :: String))
